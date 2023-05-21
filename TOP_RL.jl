a7925a7b95ba1901c3e1f51a2cdb6c1aa7026c2d
juanfran143 <82934982+juanfran143@users.noreply.github.com>
2023-05-21 16:03:03 +0200

    Parse TXT

--- TOP_RL.jl
+++ TOP_RL.jl
@@ -62,6 +62,30 @@ function simulation(edges::Dict{Int8, Dict{Int8, Float64}}, num_simulations::Int
 end
 
 
+function simulation_rl(edges::Dict{Int8, Dict{Int8, Float64}}, num_simulations::Int, capacity::Float64, routes::Array{Route,1})
+    avg_reward = []
+    for _ in 1:num_simulations
+        reward_route = []
+        for route in routes
+            dist = 0
+            first_node = route.route[1]
+            for next_node in route.route[2:end]
+                dist += modify_value_lognormal(edge_dist(edges, first_node.id, next_node.id), 0.2)
+                first_node = next_node
+            end
+
+            reward = route.reward
+            if dist > capacity
+                reward = 0
+            end
+            push!(reward_route, reward)
+        end
+        push!(avg_reward, sum(reward_route))
+    end
+
+    return mean(avg_reward)
+end
+
 
 
 
@@ -163,6 +187,51 @@ function heuristic_with_BR(n_vehicles, nodes, edges::Dict{Int8, Dict{Int8, Float
     return get_reward_and_route(sorted_routes, n_vehicles)
 end
 
+function merge_routes_rl(Node1::Node, Node2::Node, edges::Dict{Int8, Dict{Int8, Float64}}, routes::Dict{Int, Route}, max_distance::Float64, rl)
+    route1 = routes[Node1.route_id]
+    route2 = routes[Node2.route_id]
+
+    if Node1.route_id != Node2.route_id && route1.route[end-1].id == Node1.id && route2.route[2].id == Node2.id
+        # Fusionar las rutas excluyendo el nodo final de la primera ruta y el nodo de inicio de la segunda ruta
+        merged_route_nodes = vcat(route1.route[1:end-1], route2.route[2:end])
+
+        # Calcular la distancia y la recompensa total de la nueva ruta fusionada
+        merged_route_distance = route1.dist + route2.dist - edge_dist(edges, route1.route[end-1].id, route1.route[end].id) - edge_dist(edges, route2.route[1].id, route2.route[2].id) + edge_dist(edges, Node1.id, Node2.id)
+
+        merged_route_reward = route1.reward + route2.reward
+        # Verificar si la distancia total de la nueva ruta fusionada está dentro del límite de distancia máximo
+        if merged_route_distance <= max_distance
+            routes[Node1.route_id] = Route(merged_route_nodes, merged_route_distance, merged_route_reward)
+            
+            original_route_id = Node2.route_id
+            for r in routes[original_route_id].route
+                r.route_id = Node1.route_id
+            end
+            delete!(routes, original_route_id)
+
+            
+        end
+    end
+
+end
+
+function heuristic_with_BR_RL(n_vehicles, nodes, edges::Dict{Int8, Dict{Int8, Float64}}, capacity, alpha, beta, savings, rl)
+    routes = dummy_solution(nodes, edges, capacity)
+    savings = reorder_saving_list(savings, beta)
+
+    for key in keys(savings)
+        NodeX = nodes[key[1]]
+        NodeY = nodes[key[2]]
+        merge_routes_rl(NodeX, NodeY, edges, routes, capacity, rl)
+    end
+    sorted_routes = sort(collect(routes), by = x -> x[2].reward, rev = true)
+    sorted_routes = Dict(kv[1] => kv[2] for kv in sorted_routes)
+
+    return get_reward_and_route(sorted_routes, n_vehicles)
+end
+
+
+
 function create_dic_to_rl(nodes::Dict{Int64, Node}, max_distance::Float64, start_node, end_node)
     routes = Dict{Vector{Int64}, Float64}()
     intermediate_nodes = [key for key in keys(nodes) if key != start_node && key != end_node] # nodes that are not depots
@@ -199,21 +268,22 @@ function main()
     capacity = 15.0
     alpha = Float16(0.7)
     beta = Float16(0.7)
+    start_node = 1
+    end_node = 5
+    
     nodes = Dict{Int64, Node}(5 => Node(5, 10.0, 10.0, 0.0, 0), 4 => Node(4, 5.0, 3.0, 8.0, 0), 
                               2 => Node(2, 5.0, 6.0, 8.0, 0), 3 => Node(3, 2.0, 2.0, 4.0, 0), 1 => Node(1, 0.0, 0.0, 0.0, 0))
 
 
     edges = precalculate_distances(nodes::Dict{Int64, Node})
 
-    all_routes = create_dic_to_rl(nodes, capacity, 1, 5)
+    all_routes = create_dic_to_rl(nodes, capacity, start_node, end_node)
     println(all_routes)
 
     savings = calculate_savings_dict(nodes, edges, alpha)
     best_reward = 0
     best_route = Route[]
 
-    reward = 0
-    route = Route[]
     for _ in 1:100
         reward, routes = heuristic_with_BR(n_vehicles, nodes, edges, capacity, alpha, beta, savings)
         
